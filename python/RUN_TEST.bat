9c2efd3bb68c027ed37d1835ab8fb8949b147070
ZealanL <zlaferte@gmail.com>
2024-07-31 19:39:24 -0700

    Add /python to .gitignore

--- python/RUN_TEST.bat
+++ python/RUN_TEST.bat
@@ -1,4 +1,6 @@
+:e
 del rapid_learn.pyd
 copy rapid_learn.cp39-win_amd64.pyd rapid_learn.pyd
 python test.py
 pause
+goto e
--- python/test.py
+++ python/test.py
@@ -2,41 +2,60 @@ print("Importing...")
 import rapid_learn
 from rapid_learn import *
 from rapid_learn.rewards import *
-from rapid_learn.state_setter import *
+from rapid_learn.states import *
+from rapid_learn.obs import *
+from rapid_learn.acts import *
+from rapid_learn.terminals import *
+import inspect
 import traceback
+import wandb
+import numpy as np
+#from torch.multiprocessing import Process, Pool, Queue
+from multiprocessing import Process, Pool, Queue
 print("Imported!")
 
 class TestReward(RewardFunction):
 	def get_reward(self, player, state):
-		print("GetReward", player, state)
 		return 0
 
-#class Cat(Animal):
-#	def go_py(self, n_times):
-#		return "meow! " * n_times
+class TestPolicyInterface(PolicyInterface):
+	def get_actions(self, all_obs):
+		print("received observations:", all_obs)
+		return np.array([0, 0])
 
-def env_create_fn():
+def create_env_fn():
+	
+	config = EnvConfig()
+	config.reward_fn = CombinedReward([
+		(VelocityPlayerToBallReward(), 1.0),
+		(FaceBallReward(), 0.1),
+		(TestReward(), 0.2)
+	])
 
-	# This all works
-	#c = Cat()
-	#print(call_go(c))
-	#print(c.go_py(10))
-
-	print("Making reward...")
-	config = rapid_learn.EnvConfig()
-	reward_fn = TestReward()
-	print("Setting to config...")
-	config.reward_fn = reward_fn
-	config.reward_fn = VelocityReward()
-	print("Resetting...")	
-	reward_fn.reset(GameState())
-	print("Getting reward...")
-	reward_fn.get_reward(PlayerData(), GameState())
-	print("Done")
+	config.state_setter = KickoffState()
+	config.terminal_conds = [
+		NoTouchCondition(10)
+	]
+	config.action_parser = DefaultAction()
+	config.obs_builder = DefaultObs()
 	return config
 
-try:
-	rapid_learn.collector.run(env_create_fn)
-except Exception as e:
-	print("EXCEPTION:", traceback.format_exc())
+def proc_main():
+	interface = TestPolicyInterface()
+	rapid_learn.collector.run(create_env_fn, interface)
+
+def main():
+	try:
+		processes = []
+		for i in range(5):
+			p = Process(target=proc_main, args=())
+			p.start()
+		print("Processes started!")
+		print("Done!")
+	except Exception as e:
+		print("EXCEPTION:", traceback.format_exc())
+	
+if __name__ == '__main__':
+	freeze_support()
+	main()
 	
